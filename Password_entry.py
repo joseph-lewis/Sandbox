b77fbddf5e0b5970cac86a60d47811565ed1948a
joseph-lewis <42229769+joseph-lewis@users.noreply.github.com>
2018-08-16 16:40:02 +1000

    Add password check program

--- Password_entry.py
+++ Password_entry.py
@@ -1,1 +1,5 @@
-""""""Test
+password = input("Enter password: ")
+while len(password) < 3:
+    password = input("Enter password longer than 3 characters: ")
+for char in password:
+    print("*", end="")
